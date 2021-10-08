249a8b7ad8960c79700125e0808fe7769cbc34e0
georgyteterin <teterin.normal@gmail,com>
2021-10-08 11:05:52 +0300

    5th task

---
+++ random.py
@@ -0,0 +1,13 @@
+from random import randint
+n=int(input())
+k=int(input())
+right =(randint(1, n))
+while k>0:
+    answer =int(input())
+    if answer > right:
+        print ('right answer < your answer. k = ', k-1)
+    elif answer < right:
+        print ('right answer > your answer k = ', k-1)
+    elif answer == right:
+        print ('congrats!! you won')
+    k=k-1
